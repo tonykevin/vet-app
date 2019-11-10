f3f055bc0c75d57be536cdab1d65225a94a8b9f6
tonykevin <hi@tonykevin.com>
2019-11-09 19:48:53 -0500

    define wrapper for page head

--- src/components/PageHead.js
+++ src/components/PageHead.js
@@ -3,11 +3,13 @@ import imagotype from '../images/brand/vet-imagotype.svg'
 
 const PageHeader = () => (
   <header className='c-page-head'>
-    <img
-      alt='Vet imagotype'
-      className='c-page-head__imagotype'
-      src={imagotype}
-    />
+    <div className='o-wrapper--page-head'>
+      <img
+        alt='Vet imagotype'
+        className='c-page-head__imagotype'
+        src={imagotype}
+      />
+    </div>
   </header>
 )
 
